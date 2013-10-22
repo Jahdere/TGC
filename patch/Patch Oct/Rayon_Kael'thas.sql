cda5589574fa0c44025600cbac2d5d57ff8c4695
Jahdere <paul-louis.barbier@ingesup.com>
2013-10-22 15:40:04 +0200

    [SQL] Script_Target Rayon KT

---
+++ patch/Patch Oct/Rayon_Kael'thas.sql
@@ -0,0 +1,6 @@
+#Rayon Kael'thas
+INSERT INTO spell_script_target (entry, type, targetEntry) VALUES (36089, 1 , 19622);
+INSERT INTO spell_script_target (entry, type, targetEntry) VALUES (36090, 1 , 19622);
+
+#Trigger Rayon
+UPDATE creature_template SET inhabitType = 7 WHERE entry = 20602;
