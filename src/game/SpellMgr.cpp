2b2a5a8d76e3b1b77e36c902e8816fc3c9deeb27
Jahdere <paul-louis.barbier@ingesup.com>
2013-11-20 18:11:05 +0100

    [BT - Supremus] Override Charge vs Slow self

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1950,6 +1950,11 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
 					(spellInfo_2->Id == 42311  && spellInfo_1->Id == 42312))
 					return false;
 
+				//Slow Self vs Charge (Supremus)
+				if ((spellInfo_1->Id == 41581  && spellInfo_2->Id == 41922) ||  
+					(spellInfo_2->Id == 41581  && spellInfo_1->Id == 41922))
+					return false;
+
 				break;
 			}
 		case SPELLFAMILY_MAGE:
