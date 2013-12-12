3b0354fe08c1a2d331d7acfd7b7f48d89b661613
Jahdere <paul-louis.barbier@ingesup.com>
2013-12-12 14:11:04 +0100

    [Patch] Some Patch add December

---
+++ patch/Patch Dec/BT/script_armes.php
@@ -0,0 +1,195 @@
+<?php
+
+// Pool de données des set d'armes
+$data = array( 22949 => array( 'entry' => 1668, 'equipmodel1' => 42820, 'equipmodel2' => 0,
+ 'equipmodel3' => 0, 'equipinfo1' => 50267394, 'equipinfo2' => 0, 'equipinfo3' => 0, 
+ 'equipslot1' => 273,'equipslot2' => 0, 'equipslot3' => 0), 
+	22917 => array( 'entry' => 442, 'equipmodel1' => 45150, 'equipmodel2' => 45146, 'equipmodel3' => 0, 
+ 'equipinfo1' => 33490690, 'equipinfo2' => 33490690, 'equipinfo3' => 0, 'equipslot1' => 269, 
+ 'equipslot2' => 269, 'equipslot3' => 0), 22841 => array( 'entry' => 1679, 
+ 'equipmodel1' => 43903, 'equipmodel2' => 43903 ,'equipmodel3' => 0 ,'equipinfo1' => 33488898, 
+ 'equipinfo2' => 33488898, 'equipinfo3' => 0, 'equipslot1' => 781, 
+ 'equipslot2' => 78, 'equipslot3' => 0), 22871 => array( 'entry' => 428,
+ 'equipmodel1' => 45510, 'equipmodel2' => 0, 'equipmodel3' => 0, 'equipinfo1' => 50267650,
+ 'equipinfo2' => 0, 'equipinfo3' => 0,'equipslot1' => 273,'equipslot2' => 0,
+ 'equipslot3' => 0), 22952 => array( 'entry' => 450, 'equipmodel1' => 45493,
+ 'equipmodel2' => 45493, 'equipmodel3' => 0,'equipinfo1' => 33492738,
+ 'equipinfo2' => 33492738 , 'equipinfo3' => 0 , 'equipslot1' => 781 ,'equipslot2' => 781, 
+ 'equipslot3' => 0  ), 23191 => array( 'entry' => 1679,'equipmodel1' => 43903, 
+ 'equipmodel2' => 43903, 'equipmodel3' => 0 , 'equipinfo1' => 33488898, 
+ 'equipinfo2' => 33488898, 'equipinfo3' => 0 , 'equipslot1' => 781, 'equipslot2' => 781, 
+ 'equipslot3' => 0  ), 23089 => array( 'entry' => 1679, 'equipmodel1' => 43903,
+ 'equipmodel2' => 43903, 'equipmodel3' => 0 , 'equipinfo1' => 33488898, 
+ 'equipinfo2' => 33488898, 'equipinfo3' => 0 , 'equipslot1' => 781, 'equipslot2' => 781, 
+ 'equipslot3' => 0  ), 23157 => array( 'entry' => 1366,'equipmodel1' => 25278 ,
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 50268674, 'equipinfo2' => 0, 
+ 'equipinfo3' => 0 , 'equipslot1' => 529,'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 22844 => array( 'entry' => 1411 ,'equipmodel1' => 29408, 'equipmodel2' => 0 , 
+ 'equipmodel3' => 0 , 'equipinfo1' => 33489154, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 
+ 'equipslot1' => 273, 'equipslot2' => 0 , 'equipslot3' => 0  ), 23319 => array( 'entry' => 2147, 
+ 'equipmodel1' => 1768, 'equipmodel2' => 0 , 'equipmodel3' => 0 , 
+ 'equipinfo1' => 33490434, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 3,
+ 'equipslot2' => 0 , 'equipslot3' => 0  ), 22845 => array( 'entry' => 1625 ,
+ 'equipmodel1' => 41340 , 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 50268674,
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 529, 'equipslot2' => 0, 
+ 'equipslot3' => 0  ), 22847 => array( 'entry' => 425, 'equipmodel1' => 41315, 
+ 'equipmodel2' => 0 , 'equipmodel3' => 24721 ,'equipinfo1' => 33489154, 
+ 'equipinfo2' => 0 , 'equipinfo3' => 33489666,'equipslot1' => 273, 'equipslot2' => 0, 
+ 'equipslot3' => 26 ), 23374 => array( 'entry' => 518,'equipmodel1' => 44289,
+ 'equipmodel2' => 44289 ,'equipmodel3' => 0 , 'equipinfo1' => 33492738 , 
+ 'equipinfo2' => 33492738, 'equipinfo3' => 0 , 'equipslot1' => 781, 'equipslot2' => 781,
+ 'equipslot3' => 0  ), 22846 => array( 'entry' => 1366,'equipmodel1' => 25278, 
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 50268674, 'equipinfo2' => 0, 
+ 'equipinfo3' => 0 , 'equipslot1' => 529,'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 23196 => array( 'entry' => 494, 'equipmodel1' => 34296, 'equipmodel2' => 0 , 
+ 'equipmodel3' => 0 , 'equipinfo1' => 33492226, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 
+ 'equipslot1' => 1813, 'equipslot2' => 0 , 'equipslot3' => 0  ), 23235 => array( 'entry' => 505, 
+ 'equipmodel1' => 45564,'equipmodel2' => 45564, 'equipmodel3' => 0 , 
+ 'equipinfo1' => 33488898, 'equipinfo2' => 33488898, 'equipinfo3' => 0 , 
+ 'equipslot1' => 269, 'equipslot2' => 269,'equipslot3' => 0  ), 23237 => array( 'entry' => 511, 
+ 'equipmodel1' => 45557, 'equipmodel2' => 0 , 'equipmodel3' => 0 , 
+ 'equipinfo1' => 33491458, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273, 
+ 'equipslot2' => 0 , 'equipslot3' => 0  ), 23222 => array( 'entry' => 497 ,
+ 'equipmodel1' => 45549,'equipmodel2' => 45585,'equipmodel3' => 0 , 'equipinfo1' => 33492226, 
+ 'equipinfo2' => 33492226,'equipinfo3' => 0 , 'equipslot1' => 269 ,
+ 'equipslot2' => 269,'equipslot3' => 0  ), 23239 => array( 'entry' => 513,
+ 'equipmodel1' => 45544,'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33490946,
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273 ,'equipslot2' => 0, 
+ 'equipslot3' => 0  ), 23236 => array( 'entry' => 506,'equipmodel1' => 45560, 
+ 'equipmodel2' => 45559 ,'equipmodel3' => 0 , 'equipinfo1' => 33488898,
+ 'equipinfo2' => 33490436,'equipinfo3' => 0 , 'equipslot1' => 781,'equipslot2' => 1038,
+ 'equipslot3' => 0  ), 23028 => array( 'entry' => 471,'equipmodel1' => 45518,
+ 'equipmodel2' => 44026, 'equipmodel3' => 0 , 'equipinfo1' => 33490690,
+ 'equipinfo2' => 33490436,'equipinfo3' => 0 , 'equipslot1' => 781,'equipslot2' => 1038, 
+ 'equipslot3' => 0  ), 22963 => array( 'entry' => 738,'equipmodel1' => 7493,
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33488898,'equipinfo2' => 0, 
+ 'equipinfo3' => 0 , 'equipslot1' => 781,'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 22955 => array( 'entry' => 460,'equipmodel1' => 0,'equipmodel2' => 0 , 
+ 'equipmodel3' => 16752,'equipinfo1' => 0 , 'equipinfo2' => 0 , 'equipinfo3' => 33492994, 
+ 'equipslot1' => 0 , 'equipslot2' => 0 , 'equipslot3' => 25), 22873 => array( 
+ 'entry' => 433 ,'equipmodel1' => 41973,'equipmodel2' => 41973,'equipmodel3' => 0, 
+ 'equipinfo1' => 33490690, 'equipinfo2' => 33490690, 'equipinfo3' => 0 , 
+ 'equipslot1' => 781,'equipslot2' => 781, 'equipslot3' => 0  ), 22874 => array( 
+ 'entry' => 1360, 'equipmodel1' => 25155, 'equipmodel2' => 0 , 'equipmodel3' => 0,
+ 'equipinfo1' => 33490434,'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 529, 
+ 'equipslot2' => 0 , 'equipslot3' => 0  ), 22877 => array( 'entry' => 438, 
+ 'equipmodel1' => 44855, 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33490434, 
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 529, 'equipslot2' => 0, 
+ 'equipslot3' => 0  ), 23030 => array( 'entry' => 473, 'equipmodel1' => 7478,
+ 'equipmodel2' => 0 , 'equipmodel3' => 45530, 'equipinfo1' => 50267138,
+ 'equipinfo2' => 0 , 'equipinfo3' => 33493506, 'equipslot1' => 781, 'equipslot2' => 0 ,
+ 'equipslot3' => 26), 23330 => array( 'entry' => 514, 'equipmodel1' => 45518, 
+ 'equipmodel2' => 0 , 'equipmodel3' => 45530, 'equipinfo1' => 33490690,
+ 'equipinfo2' => 0 , 'equipinfo3' => 33493506, 'equipslot1' => 781, 'equipslot2' => 0, 
+ 'equipslot3' => 26 ), 22960 => array( 'entry' => 461 ,'equipmodel1' => 45526, 
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33489154, 'equipinfo2' => 0, 
+ 'equipinfo3' => 0 , 'equipslot1' => 273,'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 23172 => array( 'entry' => 1411, 'equipmodel1' => 29408, 'equipmodel2' => 0 , 
+ 'equipmodel3' => 0 , 'equipinfo1' => 33489154,'equipinfo2' => 0 , 'equipinfo3' => 0 , 
+ 'equipslot1' => 273,'equipslot2' => 0 , 'equipslot3' => 0  ), 23400 => array( 'entry' => 527, 
+ 'equipmodel1' => 45595,'equipmodel2' => 0 , 'equipmodel3' => 0 , 
+ 'equipinfo1' => 33491458, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273,
+ 'equipslot2' => 0 , 'equipslot3' => 0  ), 23403 => array( 'entry' => 531 ,
+ 'equipmodel1' => 45594,'equipmodel2' => 45594, 'equipmodel3' => 0 , 'equipinfo1' => 33492738, 
+ 'equipinfo2' => 33492738,'equipinfo3' => 0 , 'equipslot1' => 781 ,
+ 'equipslot2' => 781,'equipslot3' => 0  ), 23402 => array( 'entry' => 528,
+ 'equipmodel1' => 45568,'equipmodel2' => 45568,'equipmodel3' => 0 , 'equipinfo1' => 33490690, 
+ 'equipinfo2' => 33490690,'equipinfo3' => 0 , 'equipslot1' => 269, 
+ 'equipslot2' => 269, 'equipslot3' => 0  ), 23397 => array( 'entry' => 522,
+ 'equipmodel1' => 45579 , 'equipmodel2' => 45580, 'equipmodel3' => 0 , 'equipinfo1' => 33489922,
+ 'equipinfo2' => 33490436, 'equipinfo3' => 0 , 'equipslot1' => 781, 
+ 'equipslot2' => 1038, 'equipslot3' => 0  ), 22869 => array( 'entry' => 427,
+ 'equipmodel1' => 36036, 'equipmodel2' => 36036, 'equipmodel3' => 0 , 'equipinfo1' => 33492738, 
+ 'equipinfo2' => 33492738, 'equipinfo3' => 0 , 'equipslot1' => 781 ,
+ 'equipslot2' => 781, 'equipslot3' => 0  ), 23337 => array( 'entry' => 1688 ,
+ 'equipmodel1' => 45392, 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33490434 ,
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 529, 'equipslot2' => 0, 
+ 'equipslot3' => 0  ), 22853 => array( 'entry' => 1487, 'equipmodel1' => 34891, 
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 50268674, 'equipinfo2' => 0, 
+ 'equipinfo3' => 0 , 'equipslot1' => 529,'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 22954 => array( 'entry' => 1547 , 'equipmodel1' => 37828, 'equipmodel2' => 0 , 
+ 'equipmodel3' => 0 , 'equipinfo1' => 33490690, 'equipinfo2' => 0 , 'equipinfo3' => 0, 
+ 'equipslot1' => 781 , 'equipslot2' => 0 , 'equipslot3' => 0  ), 23339 => array( 
+ 'entry' => 517, 'equipmodel1' => 41356, 'equipmodel2' => 0 , 'equipmodel3' => 45393, 
+ 'equipinfo1' => 33488898, 'equipinfo2' => 0 , 'equipinfo3' => 50266626, 
+ 'equipslot1' => 781 ,'equipslot2' => 0 , 'equipslot3' => 15 ), 23197 => array( 'entry' => 2182, 
+ 'equipmodel1' => 44850, 'equipmodel2' => 44850 , 'equipmodel3' => 0 , 
+ 'equipinfo1' => 33490690, 'equipinfo2' => 33490690 , 'equipinfo3' => 0 , 'equipslot1' => 781, 
+ 'equipslot2' => 781, 'equipslot3' => 0  ), 23159 => array( 'entry' => 1366 , 
+ 'equipmodel1' => 25278 ,'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 50268674, 
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 529, 'equipslot2' => 0 , 
+ 'equipslot3' => 0  ), 23158 => array( 'entry' => 1366 , 'equipmodel1' => 25278,
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 50268674,'equipinfo2' => 0 , 
+ 'equipinfo3' => 0 , 'equipslot1' => 529, 'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 22945 => array( 'entry' => 449, 'equipmodel1' => 45087,'equipmodel2' => 0 , 
+ 'equipmodel3' => 0 , 'equipinfo1' => 33491458, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 
+ 'equipslot1' => 273 ,'equipslot2' => 0 , 'equipslot3' => 0  ), 22880 => array( 'entry' => 440, 
+ 'equipmodel1' => 45069,'equipmodel2' => 0 , 'equipmodel3' => 0 , 
+ 'equipinfo1' => 33490946,'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273 , 
+ 'equipslot2' => 0 , 'equipslot3' => 0  ), 22882 => array( 'entry' => 441, 
+ 'equipmodel1' => 45095, 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33491458, 
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273,'equipslot2' => 0 , 
+ 'equipslot3' => 0  ), 23147 => array( 'entry' => 492, 'equipmodel1' => 45396,
+ 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33489922, 'equipinfo2' => 0 , 
+ 'equipinfo3' => 0 , 'equipslot1' => 269, 'equipslot2' => 0 , 'equipslot3' => 0  ), 
+ 23018 => array( 'entry' => 462, 'equipmodel1' => 45110,'equipmodel2' => 0 , 
+ 'equipmodel3' => 45111, 'equipinfo1' => 33489154, 'equipinfo2' => 0 , 
+ 'equipinfo3' => 33489410, 'equipslot1' => 273, 'equipslot2' => 0 , 'equipslot3' => 271, 
+ ), 22879 => array( 'entry' => 439, 'equipmodel1' => 45073, 'equipmodel2' => 44026, 
+ 'equipmodel3' => 0 , 'equipinfo1' => 33488898, 'equipinfo2' => 33490436, 
+ 'equipinfo3' => 0 , 'equipslot1' => 269, 'equipslot2' => 1038, 'equipslot3' => 0  ), 
+ 23047 => array( 'entry' => 478 , 'equipmodel1' => 45117 , 'equipmodel2' => 45117,
+ 'equipmodel3' => 0 , 'equipinfo1' => 33490690, 'equipinfo2' => 33490690, 'equipinfo3' => 0, 
+ 'equipslot1' => 269 , 'equipslot2' => 269 , 'equipslot3' => 0  ), 23049 => array( 
+ 'entry' => 484 , 'equipmodel1' => 45118, 'equipmodel2' => 0 , 'equipmodel3' => 0 ,
+ 'equipinfo1' => 33490946, 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273 ,
+ 'equipslot2' => 0 , 'equipslot3' => 0  ), 22953 => array( 'entry' => 456,
+ 'equipmodel1' => 44738 , 'equipmodel2' => 0 , 'equipmodel3' => 0 , 'equipinfo1' => 33490690, 
+ 'equipinfo2' => 0 , 'equipinfo3' => 0 , 'equipslot1' => 273 ,'equipslot2' => 0 ,
+ 'equipslot3' => 0));
+
+
+//Connexion DB
+$bdd = new PDO('mysql:dbname=world;host=localhost', 'jahdere', 'test');
+
+// Requêtes de check si des créatures de BT ont des set d'armes liés à creature_equip_template
+$queryCheck = "SELECT e.* , c.equipment_id FROM creature_template c JOIN creature_equip_template e ON e.entry = c.equipment_id WHERE c.entry =";
+
+foreach ($data as $key => $value) {
+	$prepare_check = $bdd->query($queryCheck.$key);
+	$data_check = $prepare_check->fetchAll();
+
+	//Check si aucune créature n'est liée à creature_equip_template afin d'être sur d'update uniquement creature_equip_template_raw
+	if(!empty($data_check))
+		die('Certaines armes ne sont pas gérées dans creature_equip_template_raw :( ');
+}
+
+
+ $count = 0;
+ //Début de la transaction
+ $bdd->beginTransaction();
+ foreach($data as $key=>$value)
+ {
+ 	//On récupère l'equipment_id de la créature
+ 	$prepare = $bdd->query('SELECT equipment_id FROM creature_template WHERE entry ='.$key);
+ 	$execute_equipment_id = $prepare->fetchAll();
+ 	if(!empty($execute_equipment_id))
+ 	{
+ 		try{
+ 		//Update de l'équipement
+ 		$prepare_update = $bdd->exec('UPDATE creature_equip_template_raw SET equipmodel1 = '.$value['equipmodel1'].', equipmodel2 = '.$value['equipmodel2'].', 
+ 			equipmodel3 = '.$value['equipmodel3'].', equipinfo1 = '.$value['equipinfo1'].', equipinfo2 = '.$value['equipinfo2'].', equipinfo3 = '.$value['equipinfo3'].',
+ 			equipslot1 = '.$value['equipslot1'].', equipslot2 = '.$value['equipslot2'].', equipslot3 = '.$value['equipslot3'].' WHERE entry = '.$value['entry']);
+
+ 		$count++;
+ 		}catch(exception $e){
+ 			//Si une erreur on annule la transaction et on affiche l'exception
+ 			$bdd->rollBack();
+ 			echo $e->getMessage();
+ 		}
+ 	}
+ }
+ $bdd->commit();
+ echo "Done";
+ echo "<br/>";
+ echo "Nombre de requête éxécutées : ".$count." / 51";
---
+++ patch/Patch Dec/Hyjal/MOB_ARME_HYJAL.sql
@@ -0,0 +1,17 @@
+#Check last equipment_id in TGC DB and set it before execute this update
+
+#Pnjs in H² and A² bastion
+#UPDATE creature_template SET equipment_id = 5302 WHERE entry = 17920;
+#INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry3) VALUES (5302, 2520, 0, 0);
+#UPDATE creature_template SET equipment_id = 5303 WHERE entry = 17936;
+#INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry3) VALUES (5303, 1010, 0, 0);
+#UPDATE creature_template SET equipment_id = 5304 WHERE entry = 17935;
+#INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry3) VALUES (5304, 1010, 0, 0);
+#UPDATE creature_template SET equipment_id = 5306 WHERE entry = 3794;
+#INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry3) VALUES (5306, 1010, 0, 0);
+#UPDATE creature_template SET equipment_id = 5307 WHERE entry = 17943;
+#INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry3) VALUES (5307, 5321, 0, 4931);
+
+#Boss Kazrogal
+#UPDATE creature_template SET equipment_id = 5308 WHERE entry = 17888;
+#INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry3) VALUES (5308, 30902, 0, 0);
