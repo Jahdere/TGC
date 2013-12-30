b57dc3f2207f287a6b521bb15a661a73425e726f
Jahdere <paul-louis.barbier@ingesup.com>
2013-12-30 16:31:28 +0100

    [Patch] Last patchs december

---
+++ patch/Patch Dec/Doomwalker/hitbox_doomwalker.sql
@@ -0,0 +1,2 @@
+#DoomWalker
+UPDATE creature_model_info SET bounding_radius = 8, combat_reach = 8 WHERE modelid = 16630;
---
+++ patch/Patch Dec/Hyjal/Lesser_Doomguard_Thrash.sql
@@ -0,0 +1,2 @@
+#Lesser DoomGuard Thrash 
+UPDATE creature_ai_scripts SET action1_param1 = 3417 WHERE id = 1786403;
