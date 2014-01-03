92d88379ee26b5b2c5c0cb97aafbe51c945be408
Jahdere <paul-louis.barbier@ingesup.com>
2014-01-03 16:32:17 +0100

    [Black Temple] Indentation start

--- src/bindings/ScriptDev2/scripts/outland/black_temple/black_temple.cpp
+++ src/bindings/ScriptDev2/scripts/outland/black_temple/black_temple.cpp
@@ -1,18 +1,18 @@
 /* Copyright (C) 2006 - 2013 ScriptDev2 <http://www.scriptdev2.com/>
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 /* ScriptData
 SDName: Black_Temple
@@ -37,32 +37,32 @@ EndContentData */
 
 bool GossipHello_npc_spirit_of_olum(Player* pPlayer, Creature* pCreature)
 {
-    ScriptedInstance* pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+	ScriptedInstance* pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
 
-    if (pInstance && (pInstance->GetData(TYPE_SUPREMUS) >= DONE) && (pInstance->GetData(TYPE_NAJENTUS) >= DONE))
-        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_OLUM1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+	if (pInstance && (pInstance->GetData(TYPE_SUPREMUS) >= DONE) && (pInstance->GetData(TYPE_NAJENTUS) >= DONE))
+		pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_OLUM1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
 
-    pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
-    return true;
+	pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
+	return true;
 }
 
 bool GossipSelect_npc_spirit_of_olum(Player* pPlayer, Creature* /*pCreature*/, uint32 /*uiSender*/, uint32 uiAction)
 {
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
-        pPlayer->CLOSE_GOSSIP_MENU();
+	if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+		pPlayer->CLOSE_GOSSIP_MENU();
 
-    pPlayer->InterruptNonMeleeSpells(false);
-    pPlayer->CastSpell(pPlayer, SPELL_TELEPORT, false);
-    return true;
+	pPlayer->InterruptNonMeleeSpells(false);
+	pPlayer->CastSpell(pPlayer, SPELL_TELEPORT, false);
+	return true;
 }
 
 void AddSC_black_temple()
 {
-    Script* pNewScript;
+	Script* pNewScript;
 
-    pNewScript = new Script;
-    pNewScript->Name = "npc_spirit_of_olum";
-    pNewScript->pGossipHello = &GossipHello_npc_spirit_of_olum;
-    pNewScript->pGossipSelect = &GossipSelect_npc_spirit_of_olum;
-    pNewScript->RegisterSelf();
+	pNewScript = new Script;
+	pNewScript->Name = "npc_spirit_of_olum";
+	pNewScript->pGossipHello = &GossipHello_npc_spirit_of_olum;
+	pNewScript->pGossipSelect = &GossipSelect_npc_spirit_of_olum;
+	pNewScript->RegisterSelf();
 }
