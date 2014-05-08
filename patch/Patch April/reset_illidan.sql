c0b3483af1fc57096a8345f24218077161771a44
Jahdere <paul-louis.barbier@ingesup.com>
2014-05-08 13:40:34 +0200

    [Patchs] April Patchs

---
+++ patch/Patch April/reset_illidan.sql
@@ -0,0 +1,9 @@
+DELETE FROM creature_linking_template WHERE master_entry = 22917;
+INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range`) VALUES
+(22996, 564, 22917, 4096, 0),
+(22997, 564, 22917, 4096, 0),
+(23089, 564, 22917, 4096, 0),
+(23197, 564, 22917, 4096, 0),
+(23226, 564, 22917, 4112, 0),
+(23375, 564, 22917, 4096, 0),
+(23498, 564, 22917, 4112, 0);
