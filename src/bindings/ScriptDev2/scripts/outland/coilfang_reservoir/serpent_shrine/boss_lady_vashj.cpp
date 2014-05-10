3c836058e6f8321d4d60ff3ed178a7828cda53c3
Jahdere <paul-louis.barbier@ingesup.com>
2014-05-10 18:35:28 +0200

    [Vashj] Static charge on player

--- src/bindings/ScriptDev2/scripts/outland/coilfang_reservoir/serpent_shrine/boss_lady_vashj.cpp
+++ src/bindings/ScriptDev2/scripts/outland/coilfang_reservoir/serpent_shrine/boss_lady_vashj.cpp
@@ -338,7 +338,7 @@ struct MANGOS_DLL_DECL boss_lady_vashjAI : public ScriptedAI
 			if (m_uiStaticCharge_Timer < uiDiff)
 			{
 				//Used on random people (only 1 person at any given time) in m_uiPhases 1 and 3, it's a debuff doing 2775 to 3225 Nature damage to the target and everybody in about 5 yards around it, every 1 seconds for 30 seconds. It can be removed by Cloak of Shadows, Iceblock, Divine Shield, etc, but not by Cleanse or Dispel Magic.
-				Unit *pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0);
+				Unit *pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, SPELL_STATIC_CHARGE_TRIGGER, SELECT_FLAG_PLAYER);
 
 				//cast Static Charge every 2 seconds for 20 seconds
 				if (pTarget && !pTarget->HasAura(SPELL_STATIC_CHARGE_TRIGGER))
