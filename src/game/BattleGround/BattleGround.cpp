aa8aaa54895126c50109f729ed95e210d3427bc3
ZergRael <zergrael@gmail.com>
2014-06-30 15:38:10 +0200

    [Pet] Also remove arena preparation from pets on start

--- src/game/BattleGround/BattleGround.cpp
+++ src/game/BattleGround/BattleGround.cpp
@@ -437,7 +437,11 @@ void BattleGround::Update(uint32 diff)
                 for (BattleGroundPlayerMap::const_iterator itr = m_Players.begin(); itr != m_Players.end(); ++itr)
                 {
                     if (Player* player = sObjectMgr.GetPlayer(itr->first))
+                    {
                         player->RemoveAurasDueToSpell(SPELL_ARENA_PREPARATION);
+                        if (player->GetPet())
+                            player->GetPet()->RemoveAurasDueToSpell(SPELL_ARENA_PREPARATION);
+                    }
                 }
 
                 CheckArenaWinConditions();
@@ -1176,7 +1180,7 @@ void BattleGround::AddPlayer(Player* plr)
         plr->UnsummonPetTemporaryIfAny();
 
         if (GetStatus() == STATUS_WAIT_JOIN)                // not started yet
-		{
+        {
             plr->CastSpell(plr, SPELL_ARENA_PREPARATION, true);
             if (plr->GetPet())
                 plr->CastSpell(plr->GetPet(), SPELL_ARENA_PREPARATION, true);
