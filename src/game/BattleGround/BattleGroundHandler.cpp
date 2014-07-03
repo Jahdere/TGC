a6ca6dcbd59c06c112214141df78956b2a64c556
ZergRael <zergrael@gmail.com>
2014-07-03 15:24:37 +0200

    [PvP] Ranked arena minimap icon

--- src/game/BattleGround/BattleGroundHandler.cpp
+++ src/game/BattleGround/BattleGroundHandler.cpp
@@ -724,6 +724,9 @@ void WorldSession::HandleBattlemasterJoinArena(WorldPacket& recv_data)
         if (isRated)
             DEBUG_LOG("Battleground: arena team id %u, leader %s queued with rating %u for type %u", _player->GetArenaTeamId(arenaslot), _player->GetName(), arenaRating, arenatype);
 
+		// Minimap skirmish/ranked
+		bg->SetRated(isRated);
+
         GroupQueueInfo* ginfo = bgQueue.AddGroup(_player, grp, bgTypeId, bgBracketId, arenatype, isRated, false, arenaRating, ateamId);
         uint32 avgTime = bgQueue.GetAverageQueueWaitTime(ginfo, _player->GetBattleGroundBracketIdFromLevel(bgTypeId));
         for (GroupReference* itr = grp->GetFirstMember(); itr != NULL; itr = itr->next())
